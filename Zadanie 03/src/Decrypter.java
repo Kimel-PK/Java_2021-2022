b6bfeaac69dacc63c277521bfdce4757f19fcaac
Kimel-PK <57668948+Kimel-PK@users.noreply.github.com>
2021-10-27 22:21:50 +0200

    Koncept programu

--- Zadanie 03/src/Decrypter.java
+++ Zadanie 03/src/Decrypter.java
@@ -1,19 +1,29 @@
 import java.util.Map;
 
 class Decrypter {
-	
+    
 	public void setInputText(String encryptedDocument) {
         
+        
+        Analyze ();
     }
     
 	public Map<Character, Character> getCode() {
+        
+        
         
         return null;
     }
     
 	public Map<Character, Character> getDecode() {
         
+        // wystarczy zapisać wszystko w jednej liście a tutaj zwrócić jej odwrotność
+        
         return null;
+    }
+    
+    private void Analyze () {
+        
     }
     
 }
