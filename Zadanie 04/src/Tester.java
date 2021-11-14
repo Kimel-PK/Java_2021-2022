4b71d0d7c223aa59105057af7527eac7325f3cdc
Kimel-PK <57668948+Kimel-PK@users.noreply.github.com>
2021-11-14 15:31:20 +0100

    Zadanie 04 gotowe

--- Zadanie 04/src/Tester.java
+++ Zadanie 04/src/Tester.java
@@ -4,20 +4,25 @@ public class Tester {
 		
 		BusLine busLine = new BusLine();
 		
-		busLine.addBusLine("173", new Position2D(1, 1), new Position2D(1, 5));
+		busLine.addBusLine("a", new Position2D(1, 1), new Position2D(2, 6));
+		busLine.addLineSegment("a", new LineSegment(new Position2D(1, 1), new Position2D(8, 8)));
+		busLine.addLineSegment("a", new LineSegment(new Position2D(8, 0), new Position2D(2, 6)));
+		busLine.addLineSegment("a", new LineSegment(new Position2D(8, 8), new Position2D(8, 0)));
 		
-		busLine.addLineSegment("173", new LineSegment(new Position2D(1, 1), new Position2D(5, 5))); // 1
-		busLine.addLineSegment("173", new LineSegment(new Position2D(5, 1), new Position2D(1, 5))); // 3
-		busLine.addLineSegment("173", new LineSegment(new Position2D(5, 5), new Position2D(5, 1))); // 2
+		busLine.addBusLine("b", new Position2D(5, 8), new Position2D(8, 8));
+		busLine.addLineSegment("b", new LineSegment(new Position2D(5, 8), new Position2D(8, 8)));
 		
-		busLine.addBusLine("144", new Position2D(4, 3), new Position2D(6, 3));
-		
-		busLine.addLineSegment("144", new LineSegment(new Position2D(4, 3), new Position2D(6, 3)));
+		busLine.addBusLine("c", new Position2D(1, 1), new Position2D(4, 2));
+		busLine.addLineSegment("c", new LineSegment(new Position2D(1, 1), new Position2D(1, 4)));
+		busLine.addLineSegment("c", new LineSegment(new Position2D(1, 4), new Position2D(4, 4)));
+		busLine.addLineSegment("c", new LineSegment(new Position2D(4, 4), new Position2D(10, 4)));
+		busLine.addLineSegment("c", new LineSegment(new Position2D(10, 4), new Position2D(10, 2)));
+		busLine.addLineSegment("c", new LineSegment(new Position2D(10, 2), new Position2D(4, 2)));
 		
 		busLine.findIntersections();
 		
+		System.out.println("Linie autobusowe");
 		System.out.println(busLine.getLines() + "\n");
-		
 		System.out.println("Pozycje skrzyżowań");
 		System.out.println(busLine.getIntersectionPositions() + "\n");
 		System.out.println("Nazwy z którymi istnieją skrzyżowania");
