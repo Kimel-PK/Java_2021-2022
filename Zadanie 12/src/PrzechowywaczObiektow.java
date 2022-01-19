996cd77e4142c162800f862d46a4562145c243e6
Kimel-PK <57668948+Kimel-PK@users.noreply.github.com>
2022-01-19 21:21:14 +0100

    Zadanie 12 - gotowe

--- Zadanie 12/src/PrzechowywaczObiektow.java
+++ Zadanie 12/src/PrzechowywaczObiektow.java
@@ -1,80 +1,105 @@
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Optional;
+import java.util.Random;
 
 class PrzechowywaczObiektow implements PrzechowywaczI {
 	
 	Connection con;
 	
-	/**
-	 * Metoda ustawia połączenie do bazy danych typu SQLite.
-	 * 
-	 * @param connection referencja do utworzonego połączenia do bazy danych.
-	 */
 	public void setConnection(Connection connection) {
 		con = connection;
 	}
-
-	/**
-	 * Zleca pisanie obiektu na dysku w podanym katalogu.
-	 * 
-	 * @param path           Identyfikator katalogu, w którym ma zostać zapisany
-	 *                       obiekt.
-	 * @param obiektDoZapisu Referencja do obiektu, ktory ma zostac zapisany na
-	 *                       dysku.
-	 * @return Identyfikator obiektu. Podanie tego identyfikatora w metodzie read ma
-	 *         pozwolic na odzyskanie obiektu.
-	 * @throws IllegalArgumentException błędny identyfikator ścieżki (brak takiej w
-	 *                                  bazie) lub problem z przekazaną referencją
-	 *                                  obiektDoZapisu.
-	 */
+	
 	public int save(int path, Object obiektDoZapisu) throws IllegalArgumentException {
 		
-		// dodaj wpis do bazy
-		
-		Statement zapytanie;
-		
 		try {
-			zapytanie = con.createStatement();
-			zapytanie.executeQuery("");
-		} catch (SQLException e) {}
-		
-		
-		// zapisz plik na dysku
-		try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("objects.bin"))) {
+			
+			Random rand = new Random();
+			String nazwaPliku = Long.toString(System.currentTimeMillis()) + "-" + rand.nextInt(99) + ".plik";
+			
+			// odczytaj sciezke z bazy
+			
+			Statement zapytanie = con.createStatement();
+			ResultSet wynik = zapytanie.executeQuery("SELECT katalog FROM Katalogi WHERE idKatalogu = " + path);
+            wynik.next();
+			
+            String sciezka = "." + wynik.getString("katalog") + "/";
+            
+			// zapisz plik na dysku
+            
+            File plik = new File(sciezka + "/" + nazwaPliku);
+            System.out.println(plik.getAbsolutePath());
+            plik.createNewFile();
+			ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(plik));
 			outputStream.writeObject(obiektDoZapisu);
-		} catch (IOException e) {}
+			
+			outputStream.close();
+			
+			// dodaj wpis do bazy
+			
+			zapytanie = con.createStatement();
+			zapytanie.executeUpdate("INSERT INTO Pliki VALUES (null, " + path + ", '" + nazwaPliku + "')");
+			
+			wynik = zapytanie.getGeneratedKeys();
+            wynik.next();
+			
+			int id = wynik.getInt(1);
+			System.out.println(id);
+			
+			return id;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		return 0;
 	}
-
-	/**
-	 * Zleca odczyt obiektu o podanym id.
-	 * 
-	 * @param obiektDoOdczytu Identyfikator obiektu, ktory chcemy odzyskac.
-	 * @return Obiekt typu Optional zawierający (o ile istnieje) obiekt o podanym
-	 *         obiektDoOdczytu. W przypadku podania błędnego idektyfikatora
-	 *         obiektDoOdczytu metoda zwraca pusty obiekt Optional. Metoda
-	 *         <b>nigdy</b> nie zwraca null.
-	 */
+	
 	public Optional<Object> read(int obiektDoOdczytu) {
 		
-		// odczytaj wpis z bazy
-		
-		
-		// odczytaj plik z dysku
-		Optional<Object> obiekt = null;
-		try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream("objects.bin"))) {
+		try {
+			
+			// odczytaj wpis z bazy
+			
+			Statement zapytanie = con.createStatement();
+			ResultSet wyniki = zapytanie.executeQuery("SELECT Katalogi.katalog, Pliki.plik FROM Pliki INNER JOIN Katalogi ON Katalogi.idKatalogu = Pliki.idKatalogu WHERE idPliku = " + obiektDoOdczytu);
+			
+			if (!wyniki.next()) {
+				return Optional.empty();
+			}
+			
+			String sciezka = wyniki.getString("katalog");
+			String nazwaPliku = wyniki.getString("plik");
+			
+			// odczytaj plik z dysku
+			Optional<Object> obiekt = null;
+			ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream("." + sciezka + "/" + nazwaPliku));
 			obiekt = Optional.of((Object) inputStream.readObject());
-		} catch (IOException e) {} catch (ClassNotFoundException e) {}
+			
+			inputStream.close();
+			return obiekt;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+	 	} catch (ClassNotFoundException e) {
+	 		e.printStackTrace();
+	 	}
 		
-		return obiekt;
+		 return null;
+		 
 	}
 	
 }
--- Zadanie 12/src/Tester.java
+++ Zadanie 12/src/Tester.java
@@ -1,7 +1,68 @@
+// Autor: .𝕋𝕨𝕖𝕝𝕧𝕖𝕎𝕖𝕥
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Tester {
 	
-	public static void main(String[] args) {
-		
-	}
+    final static String url = "jdbc:mysql://localhost:3306/java_12";
+    final static String user = "root";
+    final static String password ="";
 	
+    public static Connection getConnection() throws SQLException{
+		
+        try{
+            Class.forName ("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(url, user, password);
+			
+            System.out.println("Connected!");
+            return con;
+        } catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+       
+        return null;
+    }
+
+    public static void main(String[] args) throws SQLException {
+		
+        PrzechowywaczObiektow p = new PrzechowywaczObiektow();
+        p.setConnection (getConnection ());
+		
+        for(int i = 0; i < 6; i++){
+			
+            Cos cos = new Cos(i);
+            // p.save(i % 2 + 1, cos);
+        }
+		
+        for(int i = 1; i < 7; i++){
+        	Cos cos1 = (Cos) p.read(i).get();
+        	System.out.println(cos1.val);
+        }
+        
+        try {
+        	Cos cos1 = (Cos) p.read(30).get();
+        } catch (Exception e) {
+        	System.out.println("Obiekt nie istnieje");
+        }
+		
+    }
+}
+
+class Cos implements Serializable {
+    int val;
+	
+    public Cos(int val){
+        this.val = val;
+    }
+	
+    public int getVal() {
+        return val;
+    }
 }
